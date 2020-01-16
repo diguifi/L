05f087b8e073fc2567e85ab95f91c6978c918fa1
diguifi <lumia-diguifi@hotmail.com>
2020-01-16 16:22:37 -0300

    Fix out of board rule

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -157,6 +157,19 @@ export default class Player {
       }
     }
 
+    let piecesOnBoard = 0;
+    for(let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[0].length; j++) {
+        if (matrix[i][j] == this.playerNumber) {
+          piecesOnBoard++;
+        }
+      }
+    }
+    if (piecesOnBoard != 4) {
+      matrix[0][0] = 9;
+    }
+    
+
     if (this.previousMatrixPosition.length === 0) {
       this.previousMatrixPosition = matrix.splice(0);
     } else {
